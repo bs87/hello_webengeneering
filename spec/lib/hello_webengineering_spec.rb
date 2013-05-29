72825e00d405f9c426491525d0f942c6bf7d873e
Bjoern <bs650866@fh-muenster.de>
2013-05-29 14:20:50 +0200

    hello_webengeneering2

--- spec/lib/hello_webengineering_spec.rb
+++ spec/lib/hello_webengineering_spec.rb
@@ -4,6 +4,6 @@ describe Hello_webengineering do
   it "should return data with name Hello Wolrd! This is Webengineering" do
       @Hello_webengineering.should_receive(:self).and_return("Hello Wolrd! This is Webengineering")
       answer = @Hello_webengineering.self
-      answer.should match("Hello Wolrd! This is Webengineering")
+      answer.should match("Hello Wolrd! This is Webengineering2")
   end
 end 
